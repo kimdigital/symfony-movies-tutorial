280db9e52041f2a03ae2372cf944e64f597ae9a6
kimdigital <inshal2024@gmail.com>
2024-01-26 10:50:13 +0500

    Tested the old method for annotations for methods inside a controller.
    Added route parameters to index method inside MoviesController.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -8,12 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods: ['GET', 'HEAD'])]
+    public function index($name): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => $name,
             'path' => 'src/Controller/MoviesController.php',
         ]);
     }
+
+    // /**
+    //  * oldMethod
+    //  *
+    //  * @Route("/old", name="old")
+    //  */
+    // public function oldMethod(): JsonResponse
+    // {
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller!',
+    //         'path' => 'src/Controller/MoviesController.php',
+    //     ]);
+    // }
 }
